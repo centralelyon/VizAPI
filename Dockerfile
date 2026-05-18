98e4df26b1620e530e14e6b613aa86f11ed3223d
Nako <rbelahcene10@gmail.com>
2026-05-18 10:03:18 +0200

    fix requirements and support docker

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.13.13-trixie
+
+RUN apt-get update && apt-get install -y \
+    && rm -rf /var/lib/apt/lists/*
+    
+WORKDIR /vizapi
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY pyproject.toml .
+COPY app app/
+
+EXPOSE 8000
+
+ENTRYPOINT ["sh", "-c", "fastapi run --host 0.0.0.0 --port 8000"]
--- app/core/auth/repositories_auth.py
+++ app/core/auth/repositories_auth.py
@@ -5,12 +5,12 @@ from app.core.auth import schemas_auth
 from app.core.users import models_users
 
 
-async def add_token(
-    db: AsyncSession,
-    token: schemas_auth.TokenCreate
-):
-    db_token = models_auth.Token(**token.dict())
-    db.add(db_token)
-    await db.commit()
-    await db.refresh(db_token)
-    return db_token
+# async def add_token(
+#     db: AsyncSession,
+#     token: schemas_auth.TokenCreate
+# ):
+#     db_token = models_auth.Token(**token.dict())
+#     db.add(db_token)
+#     await db.commit()
+#     await db.refresh(db_token)
+#     return db_token
